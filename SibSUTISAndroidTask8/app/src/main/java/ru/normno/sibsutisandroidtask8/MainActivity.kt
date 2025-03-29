d3d02f2155594c1dc5863d4b497a47706adf59da
NORMss <serega.b301@gmail.com>
2025-03-29 23:57:34 +0700

    Tasks 8 completed

--- SibSUTISAndroidTask8/app/src/main/java/ru/normno/sibsutisandroidtask8/MainActivity.kt
+++ SibSUTISAndroidTask8/app/src/main/java/ru/normno/sibsutisandroidtask8/MainActivity.kt
@@ -1,43 +1,67 @@
 package ru.normno.sibsutisandroidtask8
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.runBlocking
 import ru.normno.sibsutisandroidtask8.databinding.ActivityMainBinding
-import java.lang.String
-import kotlin.Int
-import kotlin.div
-import kotlin.inc
-import kotlin.rem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var mSec: Int = 0
+    private var mRunning: Boolean = false
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val view = binding.root
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-//        runBlocking {
-//            val hоurs: Int = mSec / 3600
-//            val minutes: Int = (mSec % 3600) / 60
-//            val secs: Int = mSec % 60
-//            val time: String? = String.fоrmаt("%d:%02d:%02d", hоurs, minutes, secs)
-//            timeTextView.setText(time)
-//            if (mRunning) {
-//                mSec++
-//            }
-//            handler.postDelayed(this, 1000)
-//        }
+        binding.btnStart.setOnClickListener {
+            mRunning = true
+            runTimer()
+        }
+
+        binding.btnStop.setOnClickListener {
+            mRunning = false
+        }
+
+        binding.btnReset.setOnClickListener {
+            mSec = 0
+            updateUI(binding.tvTime)
+        }
+    }
+
+    private fun runTimer() {
+        handler.post(object : Runnable {
+            override fun run() {
+                if (mRunning) {
+                    mSec++
+                    updateUI(binding.tvTime)
+                }
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+    private fun updateUI(timeTextView: TextView) {
+        val hours = mSec / 3600
+        val minutes = (mSec % 3600) / 60
+        val secs = mSec % 60
+        val time = "%d:%02d:%02d".format(hours, minutes, secs)
+        timeTextView.text = time
     }
 }
